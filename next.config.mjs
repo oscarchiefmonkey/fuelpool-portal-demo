b7bc135d4e9a454a663b15239339e582e82ee0b8
Chief Monkey <oscar.thorin@chiefmonkey.se>
2025-09-17 18:16:07 +0200

    sdfkgjlsdfg

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',                // Gör att Next.js kör `next export` och genererar statiska filer
-    basePath: '/fuelpool-portal-demo',    // Viktigt! Sätter rätt "path" under organisationen
-    assetPrefix: '/fuelpool-portal-demo/',// Gör att script och CSS laddas från rätt path
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/fuelpool-portal-demo',
+  assetPrefix: '/fuelpool-portal-demo/',
+};
+
+export default nextConfig;

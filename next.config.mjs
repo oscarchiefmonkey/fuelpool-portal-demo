3f8ada28d3314f8a80bde47d50f5f0baa6ea0908
Chief Monkey <oscar.thorin@chiefmonkey.se>
2025-09-17 18:24:30 +0200

    Fix localhost development and add dynamic GitHub Pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production';
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/fuelpool-portal-demo',
-  assetPrefix: '/fuelpool-portal-demo/',
+  // Använd basePath och assetPrefix endast för GitHub Pages deployment
+  ...(isGithubPages && {
+    basePath: '/fuelpool-portal-demo',
+    assetPrefix: '/fuelpool-portal-demo/',
+  }),
 };
 
 export default nextConfig;
